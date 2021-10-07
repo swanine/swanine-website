4083dce54ca41332da2e7b178cb54ee9f62a0b7e
swanine <haohao1500@foxmail.com>
2021-10-07 10:18:51 +0800

    feat: app.vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-view/>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: 'pressstart2p','zpix';
+  color: #2c3e50;
+}
+
+[data-theme=dark] {
+  background-color: #49308F;
+}
+</style>
