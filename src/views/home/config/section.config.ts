9e5609956e27003014084c43fd044d520f2ef679
swanine <haohao1500@foxmail.com>
2021-10-09 06:19:46 +0800

    feat: section page config

---
+++ src/views/home/config/section.config.ts
@@ -0,0 +1,4 @@
+export const sectionConfig = {
+  sectionText: 'Hey, there! Welcome to my website.',
+  sectionAbout: ' - a fanatical web developer, although it looks like I am more of a fanatical gamer'
+}
