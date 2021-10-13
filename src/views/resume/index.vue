2d550020756f7965e97aeeb6e2946cd4cb43483f
swanine <haohao1500@foxmail.com>
2021-10-13 23:45:49 +0800

    feat: resume page title component

---
+++ src/views/resume/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="overview">
+    <page-title v-bind="titleConfig"></page-title>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import pageTitle from '@/components/page-title.vue'
+import { titleConfig } from './config/title.config'
+
+export default defineComponent({
+  setup() {
+    return {
+      titleConfig
+    }
+  },
+  components: {
+    pageTitle
+  }
+})
+</script>
