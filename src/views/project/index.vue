e673fcb4d76dfa7ad51c161de4f8cf334625bc18
swanine <haohao1500@foxmail.com>
2021-10-13 23:45:24 +0800

    feat: project page content

---
+++ src/views/project/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="overview">
+    <page-title v-bind="titleConfig"></page-title>
+    <page-project></page-project>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import pageTitle from '@/components/page-title.vue'
+import { titleConfig } from './config/title.config'
+
+import pageProject from '@/components/page-project.vue'
+
+export default defineComponent({
+  setup() {
+    return {
+      titleConfig
+    }
+  },
+  components: {
+    pageTitle,
+    pageProject
+  }
+})
+</script>
